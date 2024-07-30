b2166a9512a26a5339f4457b515fe3bd978a53d9
dada <woodada.h@gmail.com>
2024-07-30 14:13:32 +0900

    jump to springboot 2-06

---
+++ src/main/java/com/ll/sapp/answer/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.ll.sapp.answer;
+
+import com.ll.sapp.answer.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ src/main/java/com/ll/sapp/question/QuestionRepository.java
@@ -0,0 +1,14 @@
+package com.ll.sapp.question;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+    Question findBySubject(String s);
+
+    Question findBySubjectAndContent(String s, String s1);
+
+    List<Question> findBySubjectLike(String s);
+}
