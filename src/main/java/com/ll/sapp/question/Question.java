742b54746ebffd1fa408af86c176874f6de97802
dada <woodada.h@gmail.com>
2024-08-01 17:26:47 +0900

    til 3-10

--- src/main/java/com/ll/sapp/question/Question.java
+++ src/main/java/com/ll/sapp/question/Question.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -30,4 +31,9 @@ public class Question {
 
 	@ManyToOne
 	private SiteUser author;
+
+	private LocalDateTime modifyDate;
+
+	@ManyToMany
+	Set<SiteUser> voter;
 }
